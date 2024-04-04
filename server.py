0412a98f6233dbe359f17bbdad7ba435c2c7cfef
BeBono <gitbebono@gmail.com>
2024-04-04 16:12:07 -0400

    Task 8: Run static code analysisin server.py

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+'''
+This module implements the server functionality for the emotion detection app.
+'''
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,19 +9,23 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def call_detector():
-
+    '''
+        Function that return the result of NLP Emotion Detector by IBM Watson.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
-    
-    if result['dominant_emotion'] == None:
+    if result['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-        
-    else:
-        return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, \
-        'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
+    return f"For the given statement, the system response is 'anger': {result['anger']}, \
+        'disgust': {result['disgust']}, 'fear': {result['fear']}, \
+        'joy': {result['joy']} and 'sadness': {result['sadness']}.\
+        The dominant emotion is {result['dominant_emotion']}."
 
 @app.route("/")
 def index_():
+    '''
+       Function that return the initial web application.
+    '''
     return render_template ("index.html")
 
 if __name__ == "__main__":
